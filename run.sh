b60b83614c517f1ae865a094333a7323ce7f02de
Harry.Liu <donote@163.com>
2018-10-29 17:02:14 +0800

    [fix] modify run config

--- run.sh
+++ run.sh
@@ -3,5 +3,4 @@ if [ ! -d $model_dir ]; then
 	mkdir $model_dir
 fi
 
-python main.py --train_file data/train.txt --dev_file data/dev.txt --test_file data/test.txt --batch_size 128 --num_epoches 2 --model_file $model_dir/model.th --model EncoderDecoderModel \
---learning_rate 0.01 --embedding_size 200 --hidden_size 200 --eval_epoch 1 --vocab_file $model_dir/vocab.pkl --use_cuda 0
+python main.py --train_file data/train.txt --dev_file data/dev.txt --test_file data/test.txt --batch_size 128 --num_epoches 100 --model_file $model_dir/model.th --model EncoderDecoderModel --learning_rate 0.01 --embedding_size 200 --hidden_size 200 --eval_epoch 1 --vocab_file $model_dir/vocab.pkl --use_cuda 1
